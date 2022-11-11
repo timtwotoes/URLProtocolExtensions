53857550fcf9416d99ed86113c048e959bec8d8a
Tim Wolff <timtwotoes@me.com>
2022-11-11 18:24:12 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# URLProtocolExtensions
+
+A description of this package.
---
+++ Sources/URLProtocolExtensions/URLProtocolExtensions.swift
@@ -0,0 +1,6 @@
+public struct URLProtocolExtensions {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
